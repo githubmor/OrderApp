4f826545a08c9e2bebc97c562acf870ec0847805
githubmor <pc2man@gmail.com>
2020-06-11 18:11:51 +0430

    new sal mali

---
+++ OrderAndisheh.Domain/UseCase/CreateNewSalMaliIntractor.cs
@@ -0,0 +1,27 @@
+﻿using OrderAndisheh.Domain.Entity;
+using OrderAndisheh.Domain.Interfaces;
+using OrderAndisheh.Domain.Repository;
+using System;
+
+namespace OrderAndisheh.Domain.UseCase
+{
+    public class CreateNewSalMaliIntractor : IRequestHandler<SalMaliEntity, Boolean>
+    {
+        private ISalMaliRepository _repository;
+
+        public CreateNewSalMaliIntractor(ISalMaliRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool Handle(SalMaliEntity data)
+        {
+            var result = _repository.CreateNewSalMali(data);
+
+            if (result)
+                return _repository.ChangeSalMali(data);
+            else
+                return false;
+        }
+    }
+}
