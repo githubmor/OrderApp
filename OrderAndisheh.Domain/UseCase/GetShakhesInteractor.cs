6a86805b4c8b557380b37c485ccb6fa5ac3108f2
githubmor <pc2man@gmail.com>
2020-06-13 11:16:13 +0430

    change mah error in range

---
+++ OrderAndisheh.Domain/UseCase/GetShakhesInteractor.cs
@@ -0,0 +1,30 @@
+﻿using OrderAndisheh.Domain.Entity;
+using OrderAndisheh.Domain.Repository;
+using System;
+
+namespace OrderAndisheh.Domain.UseCase
+{
+    public class GetShakhesInteractor : IRequestHandler<int, ShakhesEntity>
+    {
+        private IShakhesRepository _repository;
+
+        public GetShakhesInteractor(IShakhesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ShakhesEntity Handle(int mah)
+        {
+            if (IsMahInRagne(mah))
+            {
+                throw new ArgumentException("ماه در ساخص گيري در محدوده مشخص نمي باشد", "mah");
+            }
+            return _repository.getShakhesh(mah);
+        }
+
+        private bool IsMahInRagne(int mah)
+        {
+            return mah > 0 && mah < 13;
+        }
+    }
+}
--- OrderAndisheh.Domain/UseCase/ReportGenaratorInteraction.cs
+++ OrderAndisheh.Domain/UseCase/ReportGenaratorInteraction.cs
@@ -1,20 +1,20 @@
-﻿using OrderAndisheh.Domain.Interfaces;
-using OrderAndisheh.Domain.Repository;
+﻿//using OrderAndisheh.Domain.Interfaces;
+//using OrderAndisheh.Domain.Repository;
 
-namespace OrderAndisheh.Domain.UseCase
-{
-    public class ReportGenaratorInteraction : IRequestHandler<IReport, bool>
-    {
-        private IReportRepository _repository;
+//namespace OrderAndisheh.Domain.UseCase
+//{
+//    public class ReportGenaratorInteraction : IRequestHandler<IReport, bool>
+//    {
+//        private IReportRepository _repository;
 
-        public ReportGenaratorInteraction(IReportRepository repository)
-        {
-            _repository = repository;
-        }
+//        public ReportGenaratorInteraction(IReportRepository repository)
+//        {
+//            _repository = repository;
+//        }
 
-        public bool Handle(IReport data)
-        {
-            return _repository.GenerateReport(data);
-        }
-    }
-}
+//        public bool Handle(IReport data)
+//        {
+//            return _repository.GenerateReport(data);
+//        }
+//    }
+//}
